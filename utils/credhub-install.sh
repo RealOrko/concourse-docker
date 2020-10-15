8cc05e8721547bdf260f7b600b8b9a5e6187b487
Gavin van der Merwe <gavin.vandermerwe@smarsh.com>
2020-10-15 15:09:49 +0100

    Slight tweak to crehub script

--- utils/credhub-install.sh
+++ utils/credhub-install.sh
@@ -6,7 +6,8 @@ then
     wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.7.0/credhub-linux-2.7.0.tgz /app
     tar -xvf /app/credhub-linux-2.7.0.tgz
     cp /app/credhub /usr/local/bin
-    credhub --version
-    credhub api https://credhub:9000 --skip-tls-validation
-    credhub login -u credhub -p password
 fi
+
+credhub --version
+credhub api https://credhub:9000 --skip-tls-validation
+credhub login -u credhub -p password
