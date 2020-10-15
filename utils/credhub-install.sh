359256aa7642e3100be8f454c7f50c20a1c0fb34
Gavin van der Merwe <gavin.vandermerwe@smarsh.com>
2020-10-15 15:07:48 +0100

    Making sure credhub does not install all the time

--- utils/credhub-install.sh
+++ utils/credhub-install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf credhub-linux-2.7.0.*
-wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.7.0/credhub-linux-2.7.0.tgz /app
-tar -xvf /app/credhub-linux-2.7.0.tgz
-cp /app/credhub /usr/local/bin
-credhub --version
-credhub api https://credhub:9000 --skip-tls-validation
-credhub login -u credhub -p password
+if ! command -v credhub &> /dev/null
+then
+    rm -rf credhub-linux-2.7.0.*
+    wget https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.7.0/credhub-linux-2.7.0.tgz /app
+    tar -xvf /app/credhub-linux-2.7.0.tgz
+    cp /app/credhub /usr/local/bin
+    credhub --version
+    credhub api https://credhub:9000 --skip-tls-validation
+    credhub login -u credhub -p password
+fi
